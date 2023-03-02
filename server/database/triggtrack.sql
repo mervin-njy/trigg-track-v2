c3b8438d3bc41b890ed72d6441ee6f9826fcf485
mervin-njy <mervin.njy@outlook.com>
2023-03-02 13:01:46 +0800

    Drop and create dummy data in "user" table done

--- server/database/triggtrack.sql
+++ server/database/triggtrack.sql
@@ -4,6 +4,8 @@
 
 BEGIN;
 
+DROP TABLE IF EXISTS "user", "record_entry", "record", "triggers", "review", "logger_service", "comment";
+
 CREATE TABLE "user" (
     "username" VARCHAR(100) NOT NULL PRIMARY KEY UNIQUE,
     "hash" VARCHAR(100) NOT NULL,
@@ -18,24 +20,24 @@ CREATE TABLE "user" (
     CONSTRAINT "user_type_check" 
         CHECK (("user_type" = 'Health Logger'::text) OR ("user_type" = 'Service Provider'::text)),
     CONSTRAINT "access_type_check" 
-        CHECK (("access_type" = 'Private'::text) OR ("user_type" = 'Public'::text) OR ("user_type" = 'Protected'::text))
+        CHECK (("access_type" = 'Private'::text) OR ("access_type" = 'Public'::text) OR ("access_type" = 'Protected'::text))
 );
 
 CREATE TABLE "record_entry" (
-  "id" SERIAL NOT NULL PRIMARY KEY,
+  "id" SERIAL NOT NULL PRIMARY KEY UNIQUE,
   "title" VARCHAR(50) NOT NULL,
   "item" TEXT NOT NULL,
   "image_url" TEXT NULL,
-  "trigger_tag" BOOLEAN NOT NULL
+  "trigger_tag" BOOLEAN NOT NULL UNIQUE
 );
 
 CREATE TABLE "record" (
-  "logger_username" VARCHAR(100) NOT NULL PRIMARY KEY,
+  "logger_username" VARCHAR(100) NOT NULL PRIMARY KEY UNIQUE,
   "date" DATE NOT NULL,
   "type" VARCHAR(20) NOT NULL,
   "name" VARCHAR(100) NOT NULL,
   "category" VARCHAR(100) NOT NULL,
-  "record_entry_id" INT4 NOT NULL,
+  "record_entry_id" INT4 NOT NULL UNIQUE,
   CONSTRAINT "fk_username"
     FOREIGN KEY ("logger_username")
         REFERENCES "user" ("username"),
@@ -47,10 +49,10 @@ CREATE TABLE "record" (
 );
 
 CREATE TABLE "triggers" (
-  "logger_username" VARCHAR(100) NOT NULL PRIMARY KEY,
+  "logger_username" VARCHAR(100) NOT NULL PRIMARY KEY UNIQUE,
   "trigger_condition" VARCHAR(100) NOT NULL,
   "trigger_variable" VARCHAR(100) NOT NULL,
-  "trigger_id" INT4 NOT NULL,
+  "trigger_id" INT4 NOT NULL UNIQUE,
   CONSTRAINT "fk_username"
     FOREIGN KEY ("logger_username")
         REFERENCES "user" ("username"),
@@ -60,7 +62,7 @@ CREATE TABLE "triggers" (
 );
 
 CREATE TABLE "review" (
-  "date" DATE NOT NULL PRIMARY KEY,
+  "date" DATE NOT NULL PRIMARY KEY UNIQUE,
   "logger_review" TEXT NOT NULL,
   "logger_rating" INT2 NOT NULL,
   CONSTRAINT "rating_check" 
@@ -69,9 +71,9 @@ CREATE TABLE "review" (
 
 CREATE TABLE "logger_service" (
   "username" VARCHAR(100) NOT NULL UNIQUE,
-  "associated_username" VARCHAR(100) NOT NULL,
+  "associated_username" VARCHAR(100) NOT NULL UNIQUE,
   "status" VARCHAR(20) NOT NULL,
-  "review_date" DATE NULL PRIMARY KEY,
+  "review_date" DATE NULL PRIMARY KEY UNIQUE,
   CONSTRAINT "fk_username"
     FOREIGN KEY ("username")
         REFERENCES "user" ("username"),
@@ -83,10 +85,10 @@ CREATE TABLE "logger_service" (
 );
 
 CREATE TABLE "comment" (
-  "servicer_username" VARCHAR(100) NOT NULL,
+  "servicer_username" VARCHAR(100) NOT NULL UNIQUE,
   "servicer_comment" VARCHAR(255) NOT NULL,
   "servicer_response" BOOLEAN NULL,
-  "record_entry_id" INT4 NOT NULL PRIMARY KEY,
+  "record_entry_id" INT4 NOT NULL PRIMARY KEY UNIQUE,
   CONSTRAINT "fk_username"
     FOREIGN KEY ("servicer_username")
         REFERENCES "logger_service" ("username"),
@@ -95,6 +97,10 @@ CREATE TABLE "comment" (
         REFERENCES "record_entry" ("id")
 );
 
+
+COPY "user" ("username", "hash", "user_type", "access_type", "full_name", "profession", "email", "bio") FROM stdin (DELIMITER ',');
+mervin_njy,mervin123,Health Logger,Public,Ng Jian Yi Mervin,Student,mervin_njy@outlook.com,test
+\.
 
 -- COPY city (id, name, countrycode, district, population) FROM stdin;
 -- 1	Kabul	AFG	Kabol	1780000
@@ -138,7 +144,7 @@ CREATE TABLE "comment" (
 -- ALTER TABLE ONLY countrylanguage
 --     ADD CONSTRAINT countrylanguage_countrycode_fkey FOREIGN KEY (countrycode) REFERENCES country(code);
 
--- COMMIT;
+COMMIT;
 
 -- ANALYZE city;
 -- ANALYZE country;
