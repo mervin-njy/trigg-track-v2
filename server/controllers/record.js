c1ad8e09796b95c75b7d55a009afa2413af56c4b
mervin-njy <mervin.njy@outlook.com>
2023-03-05 13:16:41 +0800

    endpoint 9 /createEntry done not tested

--- server/controllers/record.js
+++ server/controllers/record.js
@@ -43,17 +43,31 @@ const createRecord = async (req, res) => {
 };
 
 const createEntry = async (req, res) => {
-  // try {
-  //   const createdRecord = await pool.query(
-  //     'INSERT INTO "record" (date, username) VALUES ($1, $2) RETURNING *',
-  //     [req.body.date, req.decoded.username]
-  //   );
-  //   console.log("created user is ", createdRecord.rows[0]);
-  //   res.json({ status: "okay", message: "record created" });
-  // } catch (error) {
-  //   console.log("PUT /logger/createRecord/", error);
-  //   res.status(400).json({ status: "error", message: "an error has occurred" });
-  // }
+  try {
+    // only allow Health loggers
+    if (req.decoded.userType !== "Health Logger") {
+      res.status(401).json({ status: "error", message: "unauthorised" });
+    }
+
+    const createdEntry = await pool.query(
+      'INSERT INTO "record" (record_date, type, name, category, title, item, image_url, trigger_tag) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *',
+      [
+        req.body.date,
+        req.body.type,
+        req.body.name,
+        req.body.category,
+        req.body.title,
+        req.body.item,
+        req.body.imageUrl,
+        req.body.triggerTag,
+      ]
+    );
+    console.log("created user is ", createdEntry.rows[0]);
+    res.json({ status: "okay", message: "record created" });
+  } catch (error) {
+    console.log("PUT /logger/createEntry/", error);
+    res.status(400).json({ status: "error", message: "an error has occurred" });
+  }
 };
 
 const displayAllRecords = () => {};
