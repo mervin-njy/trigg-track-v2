502b012366e66ba6f1bebab9b4624038071d946e
mervin-njy <mervin.njy@outlook.com>
2023-04-10 19:29:57 +0800

    display logged user details (just keys for now) at records page

--- client/src/components/records/Records.jsx
+++ client/src/components/records/Records.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const Records = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const Records = ({ loggedUserData }) => {
+  return (
+    <div>
+      {Object.keys(loggedUserData).map((element, ind) => {
+        return <h2 key={ind}>{element}</h2>;
+      })}
+    </div>
+  );
 };
 
 export default Records;
