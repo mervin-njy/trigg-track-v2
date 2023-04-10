51ee91c8890fc095262eb7fc5e2d393ee9b2de5f
mervin-njy <mervin.njy@outlook.com>
2023-04-10 19:33:27 +0800

    add some styling to refresh knowledge

--- client/src/components/records/Records.jsx
+++ client/src/components/records/Records.jsx
@@ -2,11 +2,17 @@ import React, { useState } from "react";
 
 const Records = ({ loggedUserData }) => {
   return (
-    <div>
+    <>
       {Object.keys(loggedUserData).map((element, ind) => {
-        return <h2 key={ind}>{element}</h2>;
+        return (
+          <div className="flex flex-wrap justify-between mb-8">
+            <h2 key={ind} className="tracking-wider text-4xl font-800 mx-3">
+              {element}
+            </h2>
+          </div>
+        );
       })}
-    </div>
+    </>
   );
 };
 
