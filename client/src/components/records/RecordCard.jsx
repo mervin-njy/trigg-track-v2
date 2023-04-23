95fb2a22a4bf20465e6dfadf8608fa9c6605cb79
mervin-njy <mervin.njy@outlook.com>
2023-04-24 01:48:03 +0800

    added todo to sort Record Card Display + CRUD operations on event handlers

--- client/src/components/records/RecordCard.jsx
+++ client/src/components/records/RecordCard.jsx
@@ -5,9 +5,17 @@ const RecordCard = ({ date, entries }) => {
   const [conditions, setConditions] = useState([]); // for storing variables on entries load + rerender on user changes
   const [variables, setVariables] = useState([]); // for storing variables on entries load + rerender on user changes
 
+  // event handlers -----------------------------------------------------------------------------------------------
+  // TODO:
+  // 1. prepare for handle CRUD operations (ONLY Update/Delete) on user interaction
+
   // // effects ------------------------------------------------------------------------------------------------------
   // #1 - run on mount to set conditions & variables based on entries prop
   useEffect(() => {
+    // TODO:
+    // 1. change state from array to k-v pair
+    // 2. check why this useEffect runs many times on rerender
+    // 3. sort into nested k-v pair: name > category > title: item
     entries.map((element) => {
       console.log("RecordCard, items - ", element);
       element.type === "Condition" &&
