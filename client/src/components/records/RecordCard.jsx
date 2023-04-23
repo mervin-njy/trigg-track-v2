09a95431b76b660aac34c133dda6ab5df8d38ff8
mervin-njy <mervin.njy@outlook.com>
2023-04-24 01:36:16 +0800

    useEffect to sort entries into conditions & variables

--- client/src/components/records/RecordCard.jsx
+++ client/src/components/records/RecordCard.jsx
@@ -1,19 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const RecordCard = ({ date, entries }) => {
   // states ------------------------------------------------------------------------------------------------------
-  const [conditions, setConditions] = useState(); // for storing variables on entries load + rerender on user changes
-  const [variables, setVariables] = useState(); // for storing variables on entries load + rerender on user changes
+  const [conditions, setConditions] = useState([]); // for storing variables on entries load + rerender on user changes
+  const [variables, setVariables] = useState([]); // for storing variables on entries load + rerender on user changes
+
+  // // effects ------------------------------------------------------------------------------------------------------
+  // #1 - run on mount to set conditions & variables based on entries prop
+  useEffect(() => {
+    entries.map((element) => {
+      console.log("RecordCard, items - ", element);
+      element.type === "Condition" &&
+        setConditions((prevConditions) => {
+          return [...prevConditions, element];
+        });
+
+      element.type === "Variable" &&
+        setVariables((prevVariables) => {
+          return [...prevVariables, element];
+        });
+    });
+  }, []);
 
   // render component --------------------------------------------------------------------------------------------
   return (
     <div>
       <h2 className="tracking-widest text-3xl font-medium mb-12">{date}</h2>
 
-      {/* <section className="flex flex-wrap">
+      <section className="flex flex-wrap justify-start">
         {conditions.map((condition, i) => {
           return (
-            <div key={i} className="w-2/5">
+            <div key={i} className="flex flex-wrap w-2/5">
               <h2 className="tracking-widest text-2xl font-medium">
                 {condition.title}
               </h2>
@@ -26,7 +43,7 @@ const RecordCard = ({ date, entries }) => {
 
         {variables.map((variable, i) => {
           return (
-            <div key={i} className="w-3/5">
+            <div key={i} className="flex flex-wrap  w-3/5">
               <h2 className="tracking-widest text-2xl font-medium">
                 {variable.title}
               </h2>
@@ -36,7 +53,7 @@ const RecordCard = ({ date, entries }) => {
             </div>
           );
         })}
-      </section> */}
+      </section>
     </div>
   );
 };
