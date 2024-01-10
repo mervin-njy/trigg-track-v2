7a1d4e458e1b26e6a6a5edefa0a8c7d3980a348f
mervin-njy <mervin.njy@outlook.com>
2024-01-10 17:30:50 +0800

    vercel config for pg db

--- server/database/db.js
+++ server/database/db.js
@@ -2,12 +2,16 @@ const Pool = require("pg").Pool;
 const dotenv = require("dotenv");
 dotenv.config();
 
+// const pool = new Pool({
+//   user: process.env.PGUSER,
+//   password: process.env.PGPASSWORD, // ALTER USER postgres WITH PASSWORD 'new_password';
+//   host: process.env.PGHOST,
+//   port: process.env.PGPORT,
+//   database: process.env.PGDATABASE,
+// });
+
 const pool = new Pool({
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD, // ALTER USER postgres WITH PASSWORD 'new_password';
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  database: process.env.PGDATABASE,
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 });
 
 module.exports = pool;
